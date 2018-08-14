8a01c001607a9c617324d381307b07f99f96b6b1
riefive <rie_five@yahoo.com>
2018-08-14 15:24:28 +0700

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
@@ -23,6 +24,28 @@ module.exports = {
                 exclude: /(node_modules)/,
                 test: /\.js$/
             }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                cache: true,
+                parallel: true,
+                uglifyOptions: {
+                    ecma: 6,
+                    beautify: false,
+                    compress: true,
+                    comments: false,
+                    minify: true,
+                    mangle: true,
+                    toplevel: false,
+                    keep_classnames: false,
+                    keep_fnames: false,
+                    output: {
+                        comments: false
+                    }
+                }
+            }),
         ]
     }
 }
